5c2bcc8e97fe4a8c587f7c17be1818eb337a0b24
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-06-20 09:42:03 +0200

    feat create and home buttons

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -1,112 +1,31 @@
 @extends('layouts.admin')
 
 @section('content')
-   <div class="container">
-
-      <div class="row justify-content-center">
-
-         <div class="col-md-8 mt-4">
-
-            <div class="card">
-
-               {{-- Header --}}
-               <div class="card-header d-flex justify-content-between">
-
-                  {{-- Project Table Title --}}
-                  <div class="col-9 fw-bold fs-3 text-primary">
-
-                     {{ __('Add New Project') }}
-
-                  </div>
-
-                  {{-- Button to Projects Table --}}
-                  <div class="col-3 d-flex justify-content-end align-items-end">
-
-                     <button type="button"
-                     class="btn btn-outline-primary h-75 w-100 d-flex align-items-center justify-content-center">
-      
-                     <a href="{{ route('admin.projects.index') }}">
-      
-                        <i class="fa-solid fa-angles-left"></i> Go to Projects Table
-      
-                     </a>
-      
-                  </button>
-
-                  </div>
-               </div>
-
-               {{-- Form Section --}}
-               <section class="card-body">
-
-                  <form class="border rounded p-3 my-4" action="{{ route('admin.projects.store') }}" method="POST">
-                     @csrf
-
-                     <div class="row g-3">
-
-                        {{-- Name Input --}}
-                        <div class="col-12">
-
-                           <label for="name" class="form-label fw-bold">Project Name</label>
-                           <input type="text"
-                              class="form-control
-                              @error('name')
-                              is-invalid
-                              @enderror"
-                              id="name"
-                              name="name"
-                              value="{{ old('name') }}">
-
-                           @error('name')
-                              <div class="alert alert-danger mt-1">
-                                 {{ $message }}
-                              </div>
-                           @enderror
-
-                        </div>
-
-                        {{-- Description Input --}}
-                        <div class="col-12">
-
-                           <label for="description" class="form-label">Description</label>
-                           <textarea
-                              class="form-control
-                              @error('description')
-                              is-invalid
-                              @enderror"
-                              id="description"
-                              name="description"
-                              rows="5"
-                              placeholder="Insert here a description...">{{ old('description') }}</textarea>
-
-                           @error('description')
-                              <div id="title-empty-error" class="invalid-feedback">
-                                 {{ $message }}
-                              </div>
-                           @enderror
-
-                        </div>
-
-                     </div>
-
-                     <hr class="my-4">
-
-                     {{-- Submit Button --}}
-                     <div class="col-4">
-
-                        <button class="w-100 btn btn-primary btn-lg mb-4" type="submit">Create</button>
-
-                     </div>
-
-                  </form>
-
-               </section>
-
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 mt-4">
+                <div class="d-flex justify-content-between mb-3">
+                    <h1>Create Project</h1>
+                    <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary">Projects</a>
+                </div>
+                <div class="card">
+                    <div class="card-header">{{ __('Create Project') }}</div>
+                    <div class="card-body">
+                        <form action="{{ route('admin.projects.store') }}" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="name" name="name" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Description</label>
+                                <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-
-         </div>
-
-      </div>
-
-   </div>
+        </div>
+    </div>
 @endsection
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,53 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
     <div class="container">
-
         <div class="row justify-content-center">
-
-            <div class="col-md-8 mt-4">
-
+            <div class="col-md-12 mt-4">
+                <div class="d-flex justify-content-between mb-3">
+                    <h1>Projects</h1>
+                    <div>
+                        <a href="{{ route('admin.projects.create') }}" class="btn btn-primary">Create New Project</a>
+                        
+                    </div>
+                </div>
                 <div class="card">
-
-                
-
                     <div class="card-body">
-
                         <table class="table">
-
                             <thead>
-
                                 <tr>
                                     <th scope="col">#</th>
                                     <th scope="col">Name</th>
                                     <th scope="col">Description</th>
                                     <th scope="col">Actions</th>
                                 </tr>
-
                             </thead>
-
                             <tbody>
-
                                 @foreach ($projectsArray as $project)
                                     <tr>
-                                        <th scope="row">{{ $project['slug'] }}</th>
-                                        <td>{{ $project['name'] }}</td>
-                                        <td>{{ substr($project['description'], 0, 20) }}...</td>
-                                      
+                                        <th scope="row">{{ $project->slug }}</th>
+                                        <td>{{ $project->name }}</td>
+                                        <td>{{ Str::limit($project->description, 20) }}</td>
+
                                     </tr>
                                 @endforeach
-
                             </tbody>
-
                         </table>
-
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-
     </div>
 @endsection
